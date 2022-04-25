5ebae2c8e8b4b6ee198197765639bff04ae448b6
Marina <SimashMarinka@gmail.com>
2022-04-25 21:22:30 +0500

    Examples 005

---
+++ Examples005_Name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Mari")
+{
+    Console.WriteLine("Ура, это же Марина!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
